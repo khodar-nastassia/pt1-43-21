e52487cfb447c4909ff0592ff1a4a33d0d20362f
Nastya <khodarnastassia@gmail.com>
2021-10-31 12:53:58 +0300

    task2:error had been fixed

--- src/homework4/task2.py
+++ src/homework4/task2.py
@@ -3,12 +3,13 @@
 # Используйте на предыдущий список slice чтобы получить следующий: ['ab', 'ad', 'bc'].
 # Используйте генератор списков чтобы получить следующий ['1a', '2a', '3a', '4a'].
 # Одной строкой удалите элемент  '2a' из прошлого списка и напечатайте его.
-# Скопируйте список и добавьте в него элемент '2a' так чтобы в исходном списке этого элемента не было.
+# Скопируйте список и добавьте в него элемент '2a' так чтобы в исходном списке этого элемента 
+# не было.
 
 def main():
     pairOfLetters = [["a" + x for x in "bcd"] + ["b" + x for x in "bcd"]][0]
     pairOfLetters[::2]
-    numberLetters = [str(i) +"a" for i in range(1,5)]
+    numberLetters = [str(i) + "a" for i in range(1, 5)]
     numberLetters.pop(1)
     print(numberLetters)
     numberLettersCopy = numberLetters.copy()
@@ -17,4 +18,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
