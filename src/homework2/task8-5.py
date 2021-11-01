f48cae854431b56826b3e194264e572505d1704f
Nastya <khodarnastassia@gmail.com>
2021-11-01 20:55:36 +0300

    design of descriptions has been changed

--- src/homework2/task8-5.py
+++ src/homework2/task8-5.py
@@ -1,10 +1,12 @@
-'''You need to write regex that will validate a password to make sure it meets the following
+'''
+You need to write regex that will validate a password to make sure it meets the following
 criteria:
 At least six characters long
 contains a lowercase letter
 contains an uppercase letter
 contains a number
-Valid passwords will only be alphanumeric characters.'''
+Valid passwords will only be alphanumeric characters.
+'''
 
 
 import re
