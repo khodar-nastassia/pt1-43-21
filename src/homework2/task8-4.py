782cfc06b0504953912ad7975ddfffec23f0a37b
Nastya <khodarnastassia@gmail.com>
2021-11-01 20:14:34 +0300

    task8-4

---
+++ src/homework2/task8-4.py
@@ -0,0 +1,13 @@
+''' Complete the solution so that it splits the string into pairs of two characters.
+    If the string contains an odd number of characters then it should replace the missing
+    second character of the final pair with an underscore ('_').'''
+
+def main():
+    line = str(input("введите буквы: "))
+    if len(line) % 2:
+        line += '_'
+    print([line[i:i+2] for i in range(0, len(line), 2)])
+
+
+if __name__ == "__main__":
+    main()
