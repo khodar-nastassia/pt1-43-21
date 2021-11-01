0c3328e0de21466f24cbf16d8d6fa87635533f9c
Nastya <khodarnastassia@gmail.com>
2021-11-01 20:19:58 +0300

    task8-4:whitespaces had been fixed

--- src/homework2/task8-4.py
+++ src/homework2/task8-4.py
@@ -2,11 +2,12 @@
     If the string contains an odd number of characters then it should replace the missing
     second character of the final pair with an underscore ('_').'''
 
+
 def main():
     line = str(input("введите буквы: "))
     if len(line) % 2:
         line += '_'
-    print([line[i:i+2] for i in range(0, len(line), 2)])
+    print([line[i:i + 2] for i in range(0, len(line), 2)])
 
 
 if __name__ == "__main__":
