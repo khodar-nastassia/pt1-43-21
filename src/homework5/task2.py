66a880f86894081c413262752bc309cbbc0b3e3a
Nastya <khodarnastassia@gmail.com>
2021-11-03 21:28:19 +0300

    HM5.task2

---
+++ src/homework5/task2.py
@@ -0,0 +1,45 @@
+'''
+Города
+Дан список стран и городов каждой страны. Затем даны названия городов. Для каждого города укажите,
+в какой стране он находится.
+Входные данные
+Программа получает на вход количество стран N. Далее идет N строк, каждая строка начинается с
+названия страны, затем идут названия городов этой страны. В следующей строке записано число M,
+далее идут M запросов — названия каких-то M городов, перечисленных выше.
+Выходные данные
+Для каждого из запроса выведите название страны, в котором находится данный город.
+Примеры
+Входные данные
+2
+Russia Moscow Petersburg Novgorod Kaluga
+Ukraine Kiev Donetsk Odessa
+3
+Odessa
+Moscow
+Novgorod
+Выходные данные
+Ukraine
+Russia
+Russia
+'''
+
+
+def main():
+    cityCountryMap = {}
+    listOfCities = []
+    for _ in range(int(input('Ведите число: '))):
+        country, *cities = input('Введите страну и ее города: ').split()
+        for city in cities:
+            cityCountryMap[city] = country
+            
+    for _ in range(int(input('Ведите число: '))):
+        city = str(input('Введите город: '))
+        listOfCities.append(city)
+        print()
+
+    for city in listOfCities:
+        print(cityCountryMap[city])
+
+
+if __name__ == "__main__":
+    main()
