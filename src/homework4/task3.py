19fba3f93e434d065b9b257afeead6e42a99135d
Nastya <khodarnastassia@gmail.com>
2021-11-08 20:55:55 +0300

    HW4 task3 redone

--- src/homework4/task3.py
+++ src/homework4/task3.py
@@ -1,10 +1,14 @@
 # Tuple practice.
 # Создайте кортеж из одного элемента, чтобы при итерировании по этому элементу последовательно
 # выводились значения 1, 2, 3. Убедитесь что len() исходного кортежа возвращает 1.
+
+
 def main():
-    tuple1 = (4, )
-    for i in range(1, tuple1[0]):
-        print(i)
+    tuple1 = ([1, 2, 3], )
+    for arr in tuple1:
+        for item in arr:
+            print(item)
+    print(len(tuple1))
 
 
 if __name__ == "__main__":
